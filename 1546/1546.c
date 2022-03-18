7dcaad830b7a818d1a3a826849bfe65b4c495316
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-03-18 20:25:13 +0900

    Add files via upload

---
+++ 1546/1546.c
@@ -0,0 +1,45 @@
+//
+//  main.c
+//  1546
+//
+//  Created by 심주흔 on 2022/03/14.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+double change(double max, double score){
+    return score / max * 100;
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    int sCount;                 //과목 갯수
+    int max = 0;
+    double *originalScore;
+    double *changeScore;
+    double total = 0;
+    
+    scanf("%d" , &sCount);
+    originalScore = (double*)malloc(sizeof(double) * sCount);
+    changeScore = (double*)malloc(sizeof(double) * sCount);
+    
+    for(int i=0; i<sCount; i++){
+        scanf("%lf" , &originalScore[i]);
+        if(max <= originalScore[i]){
+            max = originalScore[i];
+        }
+    }
+    
+
+    for(int i = 0; i<sCount; i++){
+        changeScore[i] = change(max, originalScore[i]);
+        total += changeScore[i];
+    }
+    
+    printf("%lf\n" , total / sCount);
+    
+    return 0;
+}
