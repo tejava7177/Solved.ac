311a810e315586b7a46002556b08ee5e8f55c410
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-03-23 18:02:02 +0900

    Add files via upload

---
+++ 10998/10998.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  10998
+//
+//  Created by 심주흔 on 2022/03/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int num1, num2;
+    
+    scanf("%d %d" , &num1, &num2);
+    
+    printf("%d\n" , num1 * num2);
+        
+    return 0;
+}
