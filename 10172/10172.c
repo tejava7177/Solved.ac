6715dab9db2bfc64a3bf4fb64018a1e3ce2faef5
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-04-08 20:21:45 +0900

    Add files via upload

---
+++ 10172/10172.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  10172
+//
+//  Created by 심주흔 on 2022/04/08.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");
+    return 0;
+}
