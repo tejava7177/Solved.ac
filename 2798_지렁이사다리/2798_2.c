faffe2d0d6e22715c8b9fd665cb79862a43adc97
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-04-16 00:10:56 +0900

    Update 2798_2.c

--- 2798_지렁이사다리/2798_2.c
+++ 2798_지렁이사다리/2798_2.c
@@ -5,18 +5,18 @@
 //  Created by 심주흔 on 2022/04/15.
 //
 
-//#include <stdio.h>
-//
-//int main(void){
-//    int a, b, v;
-//    int result;
-//    
-//    scanf("%d %d %d", &a, &b, &v);
-//    
-//    result = (v - b - 1) / (a - b) + 1;
-//    
-//    printf("%d\n" , result);
-//    
-//
-//    return 0;
-//}
+#include <stdio.h>
+
+int main(void){
+   int a, b, v;
+   int result;
+   
+   scanf("%d %d %d", &a, &b, &v);
+   
+   result = (v - b - 1) / (a - b) + 1;
+   
+   printf("%d\n" , result);
+   
+
+   return 0;
+}
