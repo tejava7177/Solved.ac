3f86d7b86c29f27786972d6aa28d0211470662f4
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-05-16 21:25:29 +0900

    Add files via upload

---
+++ 1075/1075_나누기.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  1075
+//
+//  Created by 심주흔 on 2022/05/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int num;
+    int div;
+    
+    
+    scanf("%d %d", &num, &div);
+    
+    num = num / 100;
+    num *= 100;
+    
+    for(int i = 0; i < 100; i++){
+        if(num % div == 0){
+            printf("%02d\n", i);
+            break;
+        }
+        else
+            num++;
+    }
+    return 0;
+}
