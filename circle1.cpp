c0e8bff06426bbc170d6cded229b00c427d4593b
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-10-29 23:52:24 +0900

    Add files via upload

---
+++ circle1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Circle{
+    public:
+    int radius;
+    double getArea();
+};
+
+double Circle::getArea(){
+    return 3.14*radius*radius;
+}
+
+int main(){
+    Circle donut;
+    donut.radius = 1;
+    double area= donut.getArea();
+
+    cout << "donut area is" << area << endl;
+
+    Circle pizza;
+    pizza.radius = 2;
+    area = pizza.getArea();
+    cout << "pizza area is" << area << endl;
+}
