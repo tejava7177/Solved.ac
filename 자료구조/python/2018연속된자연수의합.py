4b8b44841805e393148c03cc358bc16e85c1b415
tejava7177 <wngmstla@sch.ac.kr>
2023-12-13 13:43:38 +0900

    구간합2, 연속된자연수의 합

---
+++ 자료구조/python/2018연속된자연수의합.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+count = 1
+
+start_point = 1
+end_point = 1
+
+sum = 1
+
+while end_point < n:
+    if sum == n:
+        count += 1
+        end_point += 1
+        sum += end_point
+
+    elif sum > n:
+        sum -= start_point
+        start_point += 1
+
+
+    else:
+        end_point += 1
+        sum += end_point
+
+print(count)
