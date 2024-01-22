18bc76fba7ba68b11492f51001dfa5540d96d41d
tejava7177 <wngmstla@sch.ac.kr>
2024-01-22 20:43:43 +0900

    요소 개수 구하기(미완성)

---
+++ 자료구조/java/연결요소의개수구하기_11724.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class 연결요소의개수구하기_11724 {
+    // ArrayList : 크기를 동적으로 관리함 => 여러 정수 배열을 동적으로 관리할 수 있음.
+    static ArrayList<Integer>[] A;
+    //방문 여부를 확인 배열 생성
+    static boolean visited[];
+
+    // 입출력 동작을 수행하지만 예외가 발생하면 IOException이 실행될 수 있음.
+    public static void main(String args[]) throws IOException {
+
+    }
+}
