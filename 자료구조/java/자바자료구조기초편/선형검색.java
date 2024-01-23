26f23ba1f5f6e00d5750e54b761abf938437c5ef
tejava7177 <wngmstla@sch.ac.kr>
2024-01-23 22:55:11 +0900

    자바자료구조 기초책 선형탐색

---
+++ 자료구조/java/자바자료구조기초편/선형검색.java
@@ -0,0 +1,46 @@
+package 자바자료구조기초편;
+
+
+import java.util.Scanner;
+
+
+
+public class 선형검색 {
+        static int seqSearch(int[] a, int n, int key){
+            int i = 0;
+
+            while(true){
+                if(i == n){
+                    return -1;
+                }
+                if(a[i] == key){
+                    return i;
+                }
+                i++;
+            }
+        }
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = sc.nextInt();
+        int[] x = new int[num];
+
+        for(int i = 0; i < num; i++){
+            System.out.print("x[" + i + "] : ");
+            x[i] = sc.nextInt();
+        }
+
+        System.out.println("검색할 값 :");
+        int ky = sc.nextInt();
+
+        int idx = seqSearch(x, num, ky);
+
+        if(idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky + "은(는) x [" + idx + "]에 있습니다.");
+    }
+}
