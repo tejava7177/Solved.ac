690f085e3920a0afba99b7194651cb2026e7edfe
tejava7177 <wngmstla@sch.ac.kr>
2024-03-09 21:20:15 +0900

    손익분기점 수정

--- 자료구조/java/손익분기점_1712.java
+++ 자료구조/java/손익분기점_1712.java
@@ -1,42 +1,20 @@
 import java.util.Scanner;
 
 public class 손익분기점_1712 {
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//
-//        double a = sc.nextDouble();
-//        double b = sc.nextDouble();
-//        double c = sc.nextDouble();
-//
-//        int i = 1;
-//
-//        if(b >= c ){
-//            System.out.println(-1);
-//            return;
-//        }
-//
-//        while(c*i < (a+(b*i))){
-//            i++;
-//        }
-//
-//        System.out.println(i+1);
-//        return;
-//    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int fixedCost = sc.nextInt();
-        int variableCost = sc.nextInt();
-        int price = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        if (variableCost >= price) {
+        if (b >= c) {
             System.out.println(-1);
             return;
         }
 
-        int breakEvenPoint = fixedCost / (price - variableCost) + 1;
+        int breakEvenPoint = a / (c - b) + 1;
         System.out.println(breakEvenPoint);
     }
-
 }
